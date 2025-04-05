c8ccdede48f06bd2791cbbb01ee9fd66a1b2f73d
Shane Yost <scy@shaneyost.io>
2025-04-05 14:27:12 -0600

    Played around with cdef, small example

--- ff-example-01.lua
+++ ff-example-01.lua
@@ -1,8 +1,29 @@
 #!/usr/bin/env luajit
----@usage >lua
----     print("hello")
---- <
----
----@eval return MiniDoc.afterlines_to_code(MiniDoc.current.eval_section)
-print("hello")
---minidoc_afterlines_end
+local ffi = require("ffi")
+local FF_EXAMPLE_01 = {}
+
+function FF_EXAMPLE_01.new(def, type)
+    ffi.cdef(def)
+    return ffi.metatype(ffi.typeof(type), {
+        __index = {
+            mag = function (self)
+                return math.sqrt((self.x)^2 + (self.y)^2)
+            end,
+        },
+        __tostring = function (self)
+            return string.format("(%d, %d)", self.x, self.y)
+        end,
+    })
+end
+
+local def = [[
+typedef struct {
+    uint8_t x;
+    uint8_t y;
+} Point_t;
+]]
+
+local Point = FF_EXAMPLE_01.new(def, 'Point_t')
+local a = Point({x=1, y=2})
+print(a)
+print(a:mag())
