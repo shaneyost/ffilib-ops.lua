6c0644164b1addabdecea3b033fb7a6928f576c7
Shane Yost <scy@shaneyost.io>
2025-04-05 13:56:41 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ffilib-ops.lua
+A place for me to learn more about the FFI library w/ LuaJIT
---
+++ ff-example-01.lua
@@ -0,0 +1,8 @@
+#!/usr/bin/env luajit
+---@usage >lua
+---     print("hello")
+--- <
+---
+---@eval return MiniDoc.afterlines_to_code(MiniDoc.current.eval_section)
+print("hello")
+--minidoc_afterlines_end
