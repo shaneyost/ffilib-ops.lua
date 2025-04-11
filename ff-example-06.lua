7ce2f9953c1b04c8a141aef5379a4826d6f3081b
Shane Yost <scy@shaneyost.io>
2025-04-11 17:34:54 -0600

    created a nice example meta example for table tool

--- ff-example-06.lua
+++ ff-example-06.lua
@@ -1,20 +1,41 @@
 #!/usr/bin/env luajit
 local ffi = require("ffi")
-local FF07 = {}
+local FF06 = {}
 
---- What would it look like if we created a union on all C structures that
---- come through our table tool. This would simplify I/O operations for both
---- writing table files and displaying to console would it not?
-
---- Lets start by creating a function that takes a ctype definition and a
---- type as input. Lets go ahead and setup a metatable.
-
-function FF07.new(cdef, type)
+--- Say I wanted to simplify io such as create table files, logging or just
+--- simple hex dumps. What makes sense and what doesn't? How could I leverage
+--- metatables to implement my idea?
+---
+--- We will want the ability to add behavior to our cdata type constructor for
+--- methods and behaviors. Because we're using FFI library we use the method
+--- called `ffi.metatype` here NOT `setmetatable`.
+---
+--- Right away I know that I will represent the metamethod __index as a table.
+--- I would like to wrap ffi methods a bit but only what makes sense. So a
+--- method like `size` would be nice. Another method for returning a lua
+--- immutable string (raw byte array really) would also be nice for file i/o.
+---
+--- Ok so that takes care of alot but what about logging/dumping the raw data?
+--- I think __tostring metamethod makes the most sense here. Later I might
+--- choose too implement a separate dumping function for just dumpting the raw
+--- data and make __tostring the more elaborate dump with custom string
+--- formatting.
+---
+---@usage Lua >
+---     -- Easy file i/o
+---     file:write(p:to_byte_string())
+---     -- Printable/Resuable
+---     print(p)
+---     print(p:size())
+--- <
+---
+---@eval return MiniDoc.afterlines_to_code(MiniDoc.current.eval_section)
+function FF06.new(cdef, type)
     ffi.cdef(cdef)
     return ffi.metatype(ffi.typeof(type), {
         __index = {
             to_byte_string = function(self)
-                return ffi.string(self.raw, ffi.sizeof(self))
+                return ffi.string(self.raw, self:size())
             end,
             size = function(self)
                 return ffi.sizeof(self)
@@ -23,15 +44,13 @@ function FF07.new(cdef, type)
         __tostring = function(self)
             local out = {}
             for i = 0, self:size() - 1 do
-                table.insert(out, string.format("%02X", self.raw[i]))
+                local fmt = string.format("%02X", self.raw[i])
+                table.insert(out, fmt)
             end
             return table.concat(out, " ")
         end,
     })
 end
-
---- Cool, so now let me create the actual definition which we will specify a
---- union in there too.
 
 local cdef = [[
 typedef union
@@ -44,15 +63,12 @@ typedef union
     uint8_t raw[2];
 } __attribute__((__packed__)) Point_u;
 ]]
+---minidoc_afterlines_end
 
---- Now lets create the type constructor and then create a point (p).
-
-local Point = FF07.new(cdef, "Point_u")
+local Point = FF06.new(cdef, "Point_u")
 local p = Point({ x = 1 })
 print(p)
 print(p:size())
-
---- Lastly, need to test out our new fancy method to create table
 
 local file = assert(io.open("table.bin", "wb"))
 file:write(p:to_byte_string())
--- utils/generate-docs.lua
+++ utils/generate-docs.lua
@@ -6,4 +6,6 @@ require("mini.doc").generate({
     "ff-example-02.lua",
     "ff-example-03.lua",
     "ff-example-04.lua",
+    "ff-example-05.lua",
+    "ff-example-06.lua",
 })
