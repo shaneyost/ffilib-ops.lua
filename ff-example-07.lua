b84fcb0d6fc9aa2cf2c843f8cdc43025c88a191f
Shane Yost <scy@shaneyost.io>
2025-04-15 20:01:48 -0600

    walking through another exercise

---
+++ ff-example-07.lua
@@ -0,0 +1,27 @@
+#!/usr/bin/env luajit
+local ffi = require("ffi")
+local FF07 = {}
+
+function FF07.new(cdef, type)
+    ffi.cdef(cdef)
+    return ffi.metatype(ffi.typeof(type), {
+        __index = {
+            size = function (self) return ffi.sizeof(self) end
+        }
+    })
+end
+
+local cdef1 = [[
+typedef struct
+{
+    uint32_t x;
+    uint32_t y;
+    uint8_t  c;
+} __attribute__((__packed__)) X_t;
+]]
+
+
+local X = FF07.new(cdef1, 'X_t')
+local x = X()
+print("X_t: ", ffi.sizeof('X_t'))
+print(x:size())
